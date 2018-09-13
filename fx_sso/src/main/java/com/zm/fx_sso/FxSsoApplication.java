1379bddeeef88ed845471ee55b6538a247d7c867
min.zeng <min.zeng@heweisoft.com>
2018-09-13 19:35:29 +0800

    sso once commit

---
+++ fx_sso/src/main/java/com/zm/fx_sso/FxSsoApplication.java
@@ -0,0 +1,12 @@
+package com.zm.fx_sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxSsoApplication.class, args);
+    }
+}
